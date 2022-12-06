6f18051b3d93916a1e5c57db087f8dda22d6b112
David Svenson <davidsvenson@outlook.com>
2022-12-06 22:16:02 +0100

    Solve day 6.

---
+++ 2022/6/part_two.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+import argparse
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file_path", help="File path to read input from.")
+args = parser.parse_args()
+
+
+def get_input_lines(input_file: Path):
+    with Path(input_file).open() as f:
+        return f.readlines()
+
+
+input_lines = get_input_lines(args.file_path)
+
+MARKER_LENGTH = 14
+
+for buffer in input_lines:
+    for marker_start in range(len(buffer)):
+        marker_end = marker_start + MARKER_LENGTH
+        marker = buffer[marker_start:marker_end]
+        if len(set(marker)) == MARKER_LENGTH:
+            break
+
+    print(marker_end)
