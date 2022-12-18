d5611bb22338986f1c38d7ba53a8deb5990e339a
David Svenson <davidsvenson@outlook.com>
2022-12-18 23:17:49 +0100

    Update skel.

--- skel/part_two.py
+++ skel/part_two.py
@@ -5,12 +5,17 @@ from pathlib import Path
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file_path", help="File path to read input from.")
+parser.add_argument("--debug", action="store_true", default=False)
 args = parser.parse_args()
 
 
-def get_input_lines(input_file: Path):
+def get_input_lines(input_file: Path) -> list[str]:
     with Path(input_file).open() as f:
         return f.readlines()
 
 
-input_lines = get_input_lines(args.file_path)
+def parse_input_line(line: str) -> str:
+    return line.strip()
+
+
+input_lines = (parse_input_line(line) for line in get_input_lines(args.file_path))
