b413dad536592c9475392d6ceca4ec4ee49323bf
David Svenson <davidsvenson@outlook.com>
2023-12-03 11:30:43 +0100

    Add debug util to skel.

--- skel/part_two.py
+++ skel/part_two.py
@@ -2,11 +2,18 @@ from __future__ import annotations
 
 import argparse
 from pathlib import Path
+from typing import Any
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file_path", help="File path to read input from.")
 parser.add_argument("--debug", action="store_true", default=False)
 args = parser.parse_args()
+
+
+def debug(*print_args: Any, **print_kwargs: Any) -> None:
+    if args.debug:
+        print(*print_args, **print_kwargs)
 
 
 def get_input_lines(input_file: Path) -> list[str]:
